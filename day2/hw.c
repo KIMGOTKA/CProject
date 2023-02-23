961492e54fe1f373bf1e7a9d74bc10dde7bcf1fe
kimakfalwkf <erebus_18@naver.com>
2023-02-23 15:50:39 +0900

    첫주차 숙제 완

--- day2/hw.c
+++ day2/hw.c
@@ -11,6 +11,8 @@ int main() {
 	   힌트) % : 나머지 구하기 기호를 통해서 나누어 떨어지면 값이 0과 같다
 	   9 % 3 == 0 (9는 3으로 나누어 떨어진다, 3의 배수이다)수정해!
 */
+	
+	
 	int num;
 	int box, aa;
 
@@ -33,14 +35,46 @@ int main() {
 	   예를 들어, 126.12 ==> 6  /  33.14 ==> 3
 
 	   힌트) 나누기와 형변환을 적절히 사용
+	   */
 
+	double num2;
+	int num3, num4;
+
+	printf("임의의 실수를 입력해 주십시오.: ");
+	scanf("%lf", &num2);
+
+	num3 = num2;
+	num4 = num3 % 10;
+
+
+	printf("입력한 숫자에서 1의 자리 수: %d\n", num4);
+
+	   /*
 
 	   #5
 	   사용자로부터 초(second)를 입력 받아 [시, 분, 초] 형태로 변환해보세요
 	   예를 들어, 3665 ==> 1시간 1분 6초
 	 */
 
+	
+	int num1;
+	int sec, min1, min2, time;
+	
 
+
+	printf("초를 입력해 주십시오: ");
+	scanf("%d", &num1);
+
+	time = num1 / 3600;
+	min1 = num1 % 3600;
+	min2 = min1 / 60;
+	sec = min1 % 60;
+
+
+
+	printf("%d초는 %d시간 %d분 %d초 입니다.\n", num1, time, min2, sec);
+
+	
 
 
 
