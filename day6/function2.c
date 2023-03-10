053caaef8b432f3b4bc8d55a2ff1c2e4d1aa273f
kimakfalwkf <erebus_18@naver.com>
2023-03-10 21:29:23 +0900

    끝

--- day6/function2.c
+++ day6/function2.c
@@ -23,25 +23,33 @@ void b(char* str) {
 	printf("\n내가 전달한 문자열> %s\n", str);
  }
 //입력값 없고 반환값 있는
-void c() {
-	char ss[50] = { 0, };
+char* c() {
+	char ss[50];
 	printf("입력할 문자열을 적으세요>>");
 	scanf("%s", ss);  //char*
 	return ss;
 }
-//둘다 있는
+//입력값 있고 반환값 있는
+int d(int n1, int n2, int n3) {
+	printf("전달받은 값 : %d, %d, %d", n1, n2, n3);//전달받고자 하는 만큼 변수 생성
+	printf("\n다 더하면");
+	int str1 = n1 + n2 + n3;
+	return str1;  //함수명 앞의 자료형은 return 값의 자료형과 동일하게
+}
 
-
-int main() {
+int main99() {
 	
 	//a();  //함수의 사용법/입력 없고 반환없는
 
 
 	//b("문자열");//입력 있고 반환 없는
 
-	char* str = c();  //입력 없고 반환 있는(반환값이 있으면 변수로 받는거나 변수처럼 사용)
-printf("반환받은 값%s\n", str);
+	char *str = c();  //입력 없고 반환 있는(반환값이 있으면 변수로 받는거나 변수처럼 사용)
+printf("반환 값: %s\n", str);//오류 왜나는지 모르겠는는거 2 넘어가 시발
+printf("반환 값: %s\n", c()); //이것도 같은 오류남 넘겨 시바
 
-
+int str1 = d(5, 7, -2);  //입력있고 반환도 있다.
+printf("결과값: %d\n", str1);
+printf("결과값: %d\n", d(1,1,-1));
 return 0;
 }
